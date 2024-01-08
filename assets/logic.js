a28891452d4cbda74a711519a83d03dfd8418895
Isabel Solana Santamaria <mariaisabelsolanasantamaria.gmail.com>
2024-01-08 21:12:48 +0000

    Day/Time displayed

--- assets/logic.js
+++ assets/logic.js
@@ -13,6 +13,14 @@
 // Persist events between refreshes of a page
 
 
-
+//Display the current day at the top of the calender when a user opens the planner---->Use Day.js library to work with date and time
+//It gives you the time when the page is loaded
+function displayTime (){
+    const today = dayjs();
+    const now= dayjs().format("dddd, MMM DD YYYY  [at] HH:mm:ss");
+    $("#currentDay").text(now);
+}
+//Call the display time function-->it will calling it once each second--->it'll keep running the clock.
+setInterval(displayTime,1000);
 
 
